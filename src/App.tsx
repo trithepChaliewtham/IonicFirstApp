7717cfc25fd6ee01517a051969520a78c737f7ec
trithepChaliewtham <trithep.ch.62@ubu.ac.th>
2022-01-12 17:41:45 +0700

    Add Route path to recive parameter

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,7 @@ import { IonApp, IonRouterOutlet, IonButton, setupIonicReact } from '@ionic/reac
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import Body from './pages/Body';
-/* Core CSS required for Ionic components to work properly */
-// import '@ionic/react/css/core.css';
-
-// /* Basic CSS for apps built with Ionic */
-// import '@ionic/react/css/normalize.css';
-// import '@ionic/react/css/structure.css';
-// import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-// import '@ionic/react/css/padding.css';
-// import '@ionic/react/css/float-elements.css';
-// import '@ionic/react/css/text-alignment.css';
-// import '@ionic/react/css/text-transformation.css';
-// import '@ionic/react/css/flex-utils.css';
-// import '@ionic/react/css/display.css';
-
-/* Theme variables */
-// import './theme/variables.css';
+import Loginresult from './pages/Loginresult';
 
 setupIonicReact();
 
@@ -28,13 +11,9 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-          <Body />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
+        <Route path="/home" component={Home} />
+        <Route path="/body" component={Body} />
+        <Route path="/Loginresult/:user" component={Loginresult} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
