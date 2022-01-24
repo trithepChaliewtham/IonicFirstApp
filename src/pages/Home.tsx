7ad2e867494d07f6e22c1dac61fba833f8a7be32
trithepChaliewtham <trithep.ch.62@ubu.ac.th>
2022-01-24 19:13:19 +0700

    Add button link to Avatar page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,21 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar , IonButton } from '@ionic/react';
-// import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
-
+import { 
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol, 
+} from '@ionic/react';
+  
 const Home = ({history}:any) => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>First App</IonTitle>
+          <IonTitle>HELLO WORLD</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -18,28 +26,51 @@ const Home = ({history}:any) => {
         </IonHeader>
         {/*<ExploreContainer />*/}
       </IonContent>
-        <IonButton
-            // onClick={(e)=>{
-            //     e.preventDefault();
-            //     history.push('/home')
-            //   }
-            // }
-            href="/home"
-            routerDirection="back"
-        > 
-            To previous page
-        </IonButton>
-        <IonButton
-            // onClick={(e)=>{
-            //     e.preventDefault();
-            //     history.push('/body')
-            //   }
-            // }
-            href="/body"
-            routerDirection="forward"
-        > 
-            To next page
-        </IonButton>
+      <IonGrid>
+          <IonRow>
+              <IonCol size="12" size-sm="2" >
+                  <IonButton
+                      // onClick={(e)=>{
+                      //     e.preventDefault();
+                      //     history.push('/home')
+                      //   }
+                      // }
+                      href="/home"
+                      routerDirection="back"
+                  > 
+                      To previous page
+                  </IonButton>
+              </IonCol>
+              <IonCol size="12" size-sm="10">
+                  <IonButton
+                      // onClick={(e)=>{
+                      //     e.preventDefault();
+                      //     history.push('/body')
+                      //   }
+                      // }
+                      href="/body"
+                      routerDirection="forward"
+                  > 
+                      To next page
+                  </IonButton>
+              </IonCol>
+              <IonCol>
+                <div>--------------------------------------------------------------------------------</div>
+                <IonButton
+                      // onClick={(e)=>{
+                      //     e.preventDefault();
+                      //     history.push('/body')
+                      //   }
+                      // }
+                      href="/avatar"
+                      routerDirection="forward"
+                  > 
+                      To Avatar page
+                  </IonButton>
+              </IonCol>
+        </IonRow>
+      </IonGrid>
+      
     </IonPage>
   );
 };
